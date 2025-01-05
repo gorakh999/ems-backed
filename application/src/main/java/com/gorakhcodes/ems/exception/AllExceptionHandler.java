c6706bf124dc6a7595be138b8b79ad0b43ec6ead
Gorakh <gorakhgupta999@gmail.com>
2025-01-05 22:57:50 +0530

    Handle all Exceptions

---
+++ application/src/main/java/com/gorakhcodes/ems/exception/AllExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.gorakhcodes.ems.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AllExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
